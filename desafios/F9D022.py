7bdb6430c6c130573cabfc92b01695bd4c9a5591
Bruna Silva <brunacsilva11@outlook.com>
2023-10-02 16:27:09 -0300

    Correção

--- desafios/F9D022.py
+++ desafios/F9D022.py
@@ -6,5 +6,5 @@
 nome = input('Digite seu nome completo: ')
 print('Seu nome em maiúsculas é: {}'.format(nome.upper()))
 print('Seu nome em minúsculas é: {}'.format(nome.lower()))
-print('Seu nome tem ao todo {} letras'.format(nome.trim()))
+print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
 print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
